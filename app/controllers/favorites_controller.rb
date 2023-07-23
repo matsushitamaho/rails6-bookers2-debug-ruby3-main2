67e60a95ceb950cb48611763cb8c987fd1ca501f
mathushitamaho <mapo.maho.nissy0610@gmail.com>
2023-07-23 17:13:22 +0000

    非同期通信(いいね完了、コメントcreateまだ)

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_to request.referer
+    #redirect_to request.referer
   end
   
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
-    if favorite
-      favorite.destroy
-    else
-      flash[:alert] = "お気に入りが見つかりませんでした。"
-    end
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.find_by(user_id: current_user.id)
+    favorite.destroy
+    #redirect_to request.referer
   end
 end
--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,20 +1,22 @@
 class PostCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @user = @book.user
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.book_id = @book.id
+    @post_comment.save
+    #redirect_to book_path(@book)
   end
-  
+
   def destroy
-    comment = PostComment.find_by(id: params[:id])
-    comment.destroy
-    redirect_to request.referer
+    @post_comment = PostComment.find(params[:id]).destroy
+    @post_comment.destroy
+    @book = Book.find(params[:book_id])
+    #redirect_to request.referer
   end
 
   private
-
+  
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
