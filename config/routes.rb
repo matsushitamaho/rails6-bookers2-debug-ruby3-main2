f4a99f92dfc135265ee5a9cc0fe48b2f4fa4d26c
mathushitamaho <mapo.maho.nissy0610@gmail.com>
2023-07-27 16:35:47 +0000

    グループメール

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ Rails.application.routes.draw do
   get 'tagsearches/search', to: 'tagsearches#search'
   resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
     resource :group_users, only: [:create, :destroy]
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
 end
